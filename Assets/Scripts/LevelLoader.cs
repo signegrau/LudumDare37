aa3cd73df28a20e8f7742e26003cf85f526b0716
Mads Grau Kristensen <madsgrau@gmail.com>
2016-12-13 20:38:17 +0100

    Begun refactor of LevelManager

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using UnityEngine;
+
+public class LevelLoader
+{
+    private static readonly Dictionary<char, Tile.State> charToState = new Dictionary<char, Tile.State>
+    {
+        { '#', Tile.State.Platform },
+        { '*', Tile.State.Pickup },
+        { 's', Tile.State.Spring },
+        { 'u', Tile.State.BoostUp },
+        { 'l', Tile.State.BoostLeft },
+        { 'r', Tile.State.BoostRight },
+        { '@', Tile.State.PlayerStart },
+        { '+', Tile.State.Spike }
+    };
+
+    public static List<Tile.State[]> LoadLevel(string levelText)
+    {
+        int tileIndex = 0;
+        Tile.State tileState;
+
+        var level = new List<Tile.State[]>();
+        var state = new Tile.State[100];
+
+        foreach (char c in levelText) {
+            if (c < ' ') continue;
+
+            if (charToState.ContainsKey(char.ToLower(c)))
+            {
+                tileState = charToState[char.ToLower(c)];
+            }
+            else
+            {
+                tileState = Tile.State.Wall;
+            }
+
+            state[tileIndex] = tileState;
+
+            if (++tileIndex >= state.Length) {
+                tileIndex = 0;
+                level.Add(state);
+                state = new Tile.State[100];
+            }
+        }
+
+        return level;
+    }
+
+    /// <summary>
+    /// Load level from persistent data
+    /// </summary>
+    /// <param name="fileName"></param>
+    /// <returns></returns>
+    public static List<Tile.State[]> LoadLevelFromFile(string fileName)
+    {
+        string path = Application.persistentDataPath + "\\" + fileName;
+
+        string text = System.IO.File.ReadAllText(path);
+
+        return LoadLevel(text);
+    }
+}
--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -8,6 +8,8 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
+    public bool isEditor = false;
+
     public delegate void GameStartHandler(float time);
 
     public static event GameStartHandler OnGameStart;
@@ -37,17 +39,7 @@ public class LevelManager : MonoBehaviour
 
     private int countDeath;
 
-    private readonly Dictionary<char, Tile.State> charToState = new Dictionary<char, Tile.State>
-    {
-        { '#', Tile.State.Platform },
-        { '*', Tile.State.Pickup },
-        { 's', Tile.State.Spring },
-        { 'u', Tile.State.BoostUp },
-        { 'l', Tile.State.BoostLeft },
-        { 'r', Tile.State.BoostRight },
-        { '@', Tile.State.PlayerStart },
-        { '+', Tile.State.Spike }
-    };
+
 
 	void OnEnable() {
 		Pickup.OnPickup += AdvanceState;
@@ -66,7 +58,6 @@ public class LevelManager : MonoBehaviour
 
     private void RestartGame()
     {
-        Application.LoadLevel(Application.loadedLevel);
         SceneManager.LoadScene(0);
     }
 
@@ -76,45 +67,15 @@ public class LevelManager : MonoBehaviour
     }
 
     // Use this for initialization
-    IEnumerator Start () {
+    private IEnumerator Start () {
         tileGenerator = GetComponent<TileGenerator>();
         tiles = tileGenerator.GenerateTiles();
 
         yield return null;
 
-        allLevelStates = new List<Tile.State[]>();
-
-        // load states from file
-        // TextAsset statesFile = (TextAsset)Resources.Load("all_states");
         currentStateIndex = 0;
-        string allStateCharacters = statesFile.text;
 
-        var currentState = new Tile.State[100];
-
-        int tileIndex = 0;
-        foreach (char c in allStateCharacters) {
-            if (c < ' ') continue;
-            // Debug.Log(c);
-
-            Tile.State s;
-
-            if (charToState.ContainsKey(char.ToLower(c)))
-            {
-                s = charToState[char.ToLower(c)];
-            }
-            else
-            {
-                s = Tile.State.Wall;
-            }
-
-            currentState[tileIndex] = s;
-
-            if (++tileIndex >= currentState.Length) {
-                tileIndex = 0;
-                allLevelStates.Add(currentState);
-                currentState = new Tile.State[100];
-            }
-        }
+        allLevelStates = LevelLoader.LoadLevel(statesFile.text);
 
         player = Instantiate(playerPrefab).transform;
         player.gameObject.SetActive(false);
@@ -126,11 +87,11 @@ public class LevelManager : MonoBehaviour
     public void Update() {
         if (Input.GetKeyDown(KeyCode.U) && !isChanging)
         {
-            StartCoroutine(GotoNextState());
+            StartCoroutine(PlayerGotoNextState());
         }
     }
 
-    private IEnumerator GotoNextState()
+    private IEnumerator PlayerGotoNextState()
     {
         isChanging = true;
 
@@ -142,40 +103,41 @@ public class LevelManager : MonoBehaviour
     
     public void AdvanceState()
     {
-        var hasPlayerSpawn = false;
-
-        if (currentStateIndex < allLevelStates.Count) {
-            Tile.State[] newState = allLevelStates[currentStateIndex++];
-
-            for(int i = 0; i < newState.Length; ++i) {
-                Tile.State tileState = newState[i];
-                Tile tile = tiles[i];
-
-                if (tileState == Tile.State.PlayerStart)
-                {
-                    hasPlayerSpawn = true;
-                    playerStartPosition = tile.transform.position + new Vector3(0, 0, 0);
-                }
-                else if (tileState == Tile.State.Pickup)
-                {
-                    previousPickupPosition = pickupPosition;
-                    pickupPosition = tile.transform.position;
-                }
-
-
-                tile.GotoState(tileState);
-            }
-
-			SoundManager.single.PlayAdvanceSound();
+        if (ChangeState(currentStateIndex, !isEditor))
+        {
+            ++currentStateIndex;
         }
-        else {
-            if (OnGameEnd != null)
+        else if (OnGameEnd != null)
+        {
+            OnGameEnd(Time.time - startTime, countDeath);
+        }
+    }
+
+    public void ChangeState(Tile.State[] state, bool movePlayer)
+    {
+        bool hasPlayerSpawn = false;
+
+        for(var i = 0; i < state.Length; ++i) {
+            var tileState = state[i];
+            var tile = tiles[i];
+
+            if (tileState == Tile.State.PlayerStart)
             {
-                OnGameEnd(Time.time - startTime, countDeath);
+                hasPlayerSpawn = true;
+                playerStartPosition = tile.transform.position + new Vector3(0, 0, 0);
             }
-        }
+            else if (tileState == Tile.State.Pickup)
+            {
+                previousPickupPosition = pickupPosition;
+                pickupPosition = tile.transform.position;
+            }
 
-        if (hasPlayerSpawn)
+
+            tile.GotoState(tileState);
+        }
+        SoundManager.single.PlayAdvanceSound();
+
+        if (movePlayer && hasPlayerSpawn)
         {
             startTime = Time.time;
             if (OnGameStart != null)
@@ -186,5 +148,16 @@ public class LevelManager : MonoBehaviour
             player.transform.position = playerStartPosition;
             player.gameObject.SetActive(true);
         }
+    }
+
+    public bool ChangeState(int stateIndex, bool movePlayer)
+    {
+        if (stateIndex < allLevelStates.Count)
+        {
+            ChangeState(allLevelStates[stateIndex], movePlayer);
+            return true;
+        }
+
+        return false;
     }
 }
